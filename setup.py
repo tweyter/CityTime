eaac4facb368fc554cb24ddeedee4d1d62cf63fa
tweyter <tweyter@gmail.com>
2017-07-16 23:37:50 -0400

    Added pytest to requirements.txt Added .travis.yml Put the module and test module in separate directories.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+import unittest
 from os import path
 
+from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
@@ -8,17 +9,31 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README'), encoding='utf-8') as f:
     long_description = f.read()
 
+
+def my_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    return test_suite
+
+
+packages = ['citytime']
+
+
 setup(
     name="CityTime",
-    version="1.1.1",
+    version="1.1.3",
     description="A tool for comparing time between different locations/timezones",
     long_description=long_description,
     author="Thorsten Weyter",
     author_email="tweyter@gmail.com",
     license="MIT",
     url="https://github.com/tweyter/CityTime",
+    packages=packages,
+    test_suite='setup.my_test_suite',
+    package_dir={'citytime': 'citytime'},
     keywords="time datetime olson timezone",
-    install_requires=["pytz", "hypothesis", ],
+    install_requires=["pytz"],
+    tests_require=['hypothesis'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities"
